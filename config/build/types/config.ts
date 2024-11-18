bc15532819f96c4d28b71665de323574df35a994
tatyana.lebedeva <tatyana.lebedeva@edna.ru>
2024-11-18 18:34:43 +0100

    decompose webpack config

---
+++ config/build/types/config.ts
@@ -0,0 +1,16 @@
+export enum EBuildMode {
+    production = 'production',
+    development = 'development'
+}
+export type BuildMode = keyof typeof EBuildMode;
+export interface BuildPaths {
+    entry: string;
+    build: string;
+    html: string;
+};
+
+export interface BuildOptions {
+    mode: BuildMode;
+    paths: BuildPaths;
+    isDev: boolean
+}
