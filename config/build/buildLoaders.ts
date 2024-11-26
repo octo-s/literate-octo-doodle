41290c2ec57469619c3af1ae0c664cc6a227c690
tatyana.lebedeva <tatyana.lebedeva@edna.ru>
2024-11-26 18:48:02 +0100

    add MiniCssExtractPlugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,14 +1,14 @@
 import webpack from 'webpack';
+import {BuildOptions} from "./types/config";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
-export function buildLoaders(): webpack.RuleSetRule[]  {
+export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[]  {
     const cssLoaders =  {
             test: /\.s[ac]ss$/i,
             use: [
-                // Creates `style` nodes from JS strings
-                "style-loader",
-                // Translates CSS into CommonJS
+                // в деве не будет отдельных css файлов
+                isDev ? "style-loader" : MiniCssExtractPlugin.loader,
                 "css-loader",
-                // Compiles Sass to CSS
                 "sass-loader",
             ],
         };
