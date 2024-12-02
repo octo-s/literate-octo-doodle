8c3621aa81320537718b68cae59d0d54c9ab2bfb
tatyana.lebedeva <tslebedeva3103@gmail.com>
2024-12-02 23:51:33 +0100

    Updates

---
+++ 749.7f17a904fa3933b2bcde.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkulbi=self.webpackChunkulbi||[]).push([[749],{749:(e,u,n)=>{n.r(u),n.d(u,{default:()=>c});var s=n(848);const c=function(){return(0,s.jsx)("div",{children:(0,s.jsx)("h1",{children:"AboutPage"})})}}}]);
--- 749.870543577d1401f81f8f.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkulbi=self.webpackChunkulbi||[]).push([[749],{749:(u,e,n)=>{n.r(e),n.d(e,{default:()=>t});var s=n(848);const t=function(){return(0,s.jsx)("div",{children:"AboutPage"})}}}]);
