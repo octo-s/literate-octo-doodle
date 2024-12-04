1e29c2e76efa70e30239294d9c9675150ec89445
tatyana.lebedeva <tslebedeva3103@gmail.com>
2024-12-04 17:56:17 +0100

    add navbar widget & applink ui element

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,17 @@
-import React, {Suspense} from 'react';
+import React from 'react';
 import './styles/index.scss'
-import {Link, Route, Routes} from "react-router-dom";
 import {useTheme} from "app/providers/ThemeProvider/lib/useTheme";
 import {classNames} from "shared/classNames/classNames";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
 import {AppRouter} from "app/providers/router";
-
-const PATHS = {
-    main: '/',
-    about: `/about/`
-}
+import {Navbar} from "widgets/Navbar";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <button onClick={toggleTheme}>Сменить тему</button>
-            <Link className="link" to={PATHS.main}>Главная</Link>
-            <Link className="link" to={PATHS.about}>О сайте</Link>
+            <Navbar />
+            <button className={classNames('button')} onClick={toggleTheme}>Сменить тему</button>
             <AppRouter/>
         </div>
     );
---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+import {RoutePath} from "shared/definitions";
+import {classNames} from "shared/classNames/classNames";
+import cls from "./Navbar.module.scss"
+import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
+interface NavbarProps {
+    className?: string;
+}
+export const Navbar = ({className}:NavbarProps) => {
+    return (
+        <div className={classNames(cls.Navbar, {}, [className])}>
+            <div className={cls.links}>
+                <AppLink theme={AppLinkTheme.SECONDARY} className={classNames(cls.link)} to={RoutePath.main}>Главная</AppLink>
+                <AppLink theme={AppLinkTheme.SECONDARY} className={classNames(cls.link)} to={RoutePath.about}>О сайте</AppLink>
+            </div>
+
+        </div>
+    );
+};
+
