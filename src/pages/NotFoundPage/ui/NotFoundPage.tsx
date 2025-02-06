ce62e14fe5b3d2f9325e7129b04386ccd74f4d9f
tatyana.lebedeva <tslebedeva3103@gmail.com>
2025-02-06 10:28:03 +0100

    remove svg background

--- src/pages/NotFoundPage/ui/NotFoundPage.tsx
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -11,8 +11,9 @@ export const NotFoundPage = ({className}:NotFoundPageProps) => {
     const { t } = useTranslation('notFound');
 
     return (
-        <div className={classNames(cls.NotFoundPage, {}, [className])}
-            style={{ backgroundImage : `url("./images/bubbles.svg")`}}>
+        <div className={classNames(cls.NotFoundPage, {}, [className])}>
+            {/* todo add background */}
+            {/* style={{ backgroundImage : `url("./images/bubbles.svg")`}}> */}
             <h1>{t('title')}</h1>
         </div>
     );
